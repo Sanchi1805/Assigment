ac0215599f9d03291701e55069cf87ed49c90280
Sanchi <mamtamishra.msp98@gmail.com>
2022-06-23 14:32:03 +0530

    assigment done

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function myfun(){
+    var x = document.getElementById("form");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+//   close a from
+
+
+
+
+// function myclose(){
+//     const form = document.getElementById("from");
+
+//     form.style.display=="none";
+// }
